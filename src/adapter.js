4c08c9838a7cdeaa5a62b1cac307c561b8f9c080
Alexandre Garrec <garrec.alexandre@gmail.com>
2016-06-08 19:36:35 +0200

    husky precomit - eslint conf

--- src/adapter.js
+++ src/adapter.js
@@ -1,4 +1,4 @@
-import invariant from 'invariant'
+//import invariant from 'invariant'
 
 export class Model {
   constructor(type, converter) {
@@ -31,21 +31,21 @@ export class ArrayOf {
 
 const adapter = (data = [], model, memo = {}, parent = false) => {
   if (model.getType()) {
-      const items = data.reduce((previous, current) => {
-        let temp = model.getConverter()(current, parent)
+    const items = data.reduce((previous, current) => {
+      let temp = model.getConverter()(current, parent)
+      
+      previous[temp.id] = Object.keys(temp).reduce((p, key) => {
+        const value = temp[key]
+        if(value instanceof ArrayOf) {
+          adapter(value.getData(), value.getType(), memo, current)
+          p[key] = value.data.map(d => d.id)
+        }
+        else p[key] = value
         
-        previous[temp.id] = Object.keys(temp).reduce((p, key) => {
-          const value = temp[key]
-          if(value instanceof ArrayOf) {
-            adapter(value.getData(), value.getType(), memo, current)
-            p[key] = value.data.map(d => d.id)
-          }
-          else p[key] = value
-          
-          return p
-        }, {})
-        return previous
+        return p
       }, {})
+      return previous
+    }, {})
       
     const _type = model.getType()
     if(isObj(memo[_type])) Object.assign( memo[_type],  items)
