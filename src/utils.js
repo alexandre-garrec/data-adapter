8d1abaef960decd0b15a636aa2c45f7c063b7b06
Alexandre Garrec <garrec.alexandre@gmail.com>
2016-06-08 20:30:26 +0200

    update: mapObject

--- src/utils.js
+++ src/utils.js
@@ -1,8 +1,8 @@
 export const isObj = obj => typeof obj === 'object'
 
 export const mapObject = (obj, funct) => (
-  Object.keys(obj).reduce((previous, key) => {
-    previous[key] = funct(obj[key], key)
+  Object.keys(obj).reduce((previous, key, index) => {
+    previous[key] = funct(obj[key], key, index)
     return previous
   }, {})
 )
